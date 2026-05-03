ce71465650462565ba1caedc586d7e0fee8c8c52
Kunj Maheshwari <kunjm@outlook.com>
2026-05-03 17:14:38 +0530

    Update global styles and enhance homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button variant="outline">Click me</Button>
     </div>
   );
 }
