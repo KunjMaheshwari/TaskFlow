cf9d4b5c5e5e4635a8ccf40b5319bcfab29185f6
Kunj Maheshwari <kunjm@outlook.com>
2026-05-03 20:49:00 +0530

    feat: implement CRUD operations for todos and add validation schemas

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import CreateTodo from "@/components/create-todo";
+import TodoList from "@/components/todo-list";
 import { Button } from "@/components/ui/button";
 import UserDetails from "@/components/user-details";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <UserDetails />
-    </div>
+   <div className="flex flex-col gap-4 py-10 px-10">
+    <CreateTodo/>
+    <TodoList/>
+   </div>
   );
 }
